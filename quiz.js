434bff14e2257522649e60567684f54b22ac71b5
Alex Singleton <alexanderjsingleton@gmail.com>
2015-09-21 17:17:47 -0500

    CSS, boilerplate and first question constructed in quiz.js

---
+++ quiz.js
@@ -0,0 +1,8 @@
+// quiz begins, no answers correct
+var correct = 0;
+
+// ask questions
+var answer1 = prompt("Name a programming language that's also a gem")
+if ( answer1.toUpperCase() === 'RUBY') {
+  correct += 1;
+}
