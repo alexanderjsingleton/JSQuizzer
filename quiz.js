48c82baea889299ed09ded5f814f1167777c898a
Alex Singleton <alexanderjsingleton@gmail.com>
2015-09-21 17:22:23 -0500

    finished questions

--- quiz.js
+++ quiz.js
@@ -6,3 +6,23 @@ var answer1 = prompt("Name a programming language that's also a gem")
 if ( answer1.toUpperCase() === 'RUBY') {
   correct += 1;
 }
+var answer2 = prompt("Name a programming language that's also a snake")
+if ( answer2.toUpperCase() === 'PYTHON') {
+  correct += 1;
+}
+var answer3 = prompt("What language do you use to style web pages?")
+if ( answer3.toUpperCase() === 'CSSS') {
+  correct += 1;
+}
+var answer4 = prompt("What languagedo you use to build the structure of web pages")
+if ( answer4.toUpperCase() === 'HTML') {
+  correct += 1;
+}
+var answer5 = prompt("What lanugage do you use to add behavior or interactivity to a web page?")
+if ( answer5.toUpperCase() === 'JAVASCRIPT') {
+  correct += 1;
+}
+
+// output results
+
+
